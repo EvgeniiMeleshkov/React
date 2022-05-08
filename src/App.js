ddf2a43a012c5107f4183426ee5dccadcc759f1c
Evgenii <emeleshkov@mail.ru>
2022-05-08 18:08:23 +0300

    testing ProfileStatus using react-test-renderer

--- src/App.js
+++ src/App.js
@@ -4,16 +4,17 @@ import Navbar from './components/Navbar/Navbar'
 import News from './components/Navbar/Navbar'
 import Music from './components/Navbar/Navbar'
 import Settings from './components/Navbar/Navbar'
-import {Route, withRouter} from "react-router-dom";
+import {BrowserRouter, Route, withRouter} from "react-router-dom";
 import DialogsContainer from "./components/Dialogs/DialogsContainer";
 import UsersContainer from "./components/Users/UsersContainer";
 import ProfileContainer from "./components/Profile/ProfileContainer";
 import HeaderContainer from "./components/Header/HeaderContainer";
 import LoginPage from "./components/Login/Login";
-import {connect} from "react-redux";
+import {connect, Provider} from "react-redux";
 import {compose} from "redux";
 import {initializeApp} from "./redux/appReducer";
 import Preloader from "./components/common/Preloader/Preloader";
+import store from "./redux/reduxStore";
 
 
 class App extends React.Component {
@@ -57,6 +58,17 @@ const mapStateToProps = (state) => ({
     initialized: state.app.initialized
 })
 
-export default compose(
+
+let AppContainer = compose(
     withRouter,
     connect(mapStateToProps, {initializeApp}))(App)
+
+const SamuraiJSApp = (props) => {
+    return <BrowserRouter>
+        <Provider store={store}>
+            <AppContainer />
+        </Provider>
+    </BrowserRouter>
+}
+
+export default SamuraiJSApp
